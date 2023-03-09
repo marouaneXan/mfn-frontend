65f3231fd398ea55b1481462a92650b03ca8a15e
marouaneXan <marouanezahaoui835@gmail.com>
2023-03-09 19:56:44 +0100

    create a spinner component

---
+++ components/Spinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ActivityIndicator } from 'react-native';
+
+const Spinner = () => {
+    return (
+        <ActivityIndicator color="#0000ff" />
+    );
+};
+
+export default Spinner;
