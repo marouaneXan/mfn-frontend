2ef59a656a8a2c7f3b861df135e7dc50fa5db7d9
marouaneXan <marouanezahaoui835@gmail.com>
2023-03-09 19:27:41 +0100

    setup navigation routes

--- App.js
+++ App.js
@@ -1,18 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import RootStack from './navigators';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>mfn</Text>
-    </View>
+    <RootStack/>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ navigators/index.js
@@ -0,0 +1,16 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
+import Register from '../screens/Register';
+import Login from '../screens/Login';
+const Stack = createNativeStackNavigator();
+const RootStack=()=>{
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name='Register' component={Register} options={{ headerShown: false }}  />
+                <Stack.Screen name='Login' component={Login} options={{ headerShown: false }}  />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+export default RootStack
